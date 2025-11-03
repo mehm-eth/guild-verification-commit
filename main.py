b010ca1c74b38d4f3a63356c97cfb6d32f49673d
mehmeth <137512931+mehm-eth@users.noreply.github.com>
2025-11-03 08:50:48 +0300

    Fix CSS styling bug on mobile devices

--- main.py
+++ main.py
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2025-11-03T05:49:28.059Z
-Commit: Resolve null pointer exception in user service
+Timestamp: 2025-11-03T05:50:46.989Z
+Commit: Fix CSS styling bug on mobile devices
