9854d74c106f652288bff1923d755e86b6c1387a
mehmeth <137512931+mehm-eth@users.noreply.github.com>
2025-11-06 14:35:34 +0300

    Fix CSS styling bug on mobile devices

--- app.js
+++ app.js
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2025-11-06T11:35:23.047Z
-Commit: Patch API endpoint error handling
+Timestamp: 2025-11-06T11:35:34.015Z
+Commit: Fix CSS styling bug on mobile devices
