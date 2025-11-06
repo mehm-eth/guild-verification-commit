b0ed473d26e93b08e7ea3db268af77f46e4fa358
mehmeth <137512931+mehm-eth@users.noreply.github.com>
2025-11-06 14:33:04 +0300

    Fix memory leak in event handlers

--- helpers.ts
+++ helpers.ts
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2025-11-06T11:31:15.685Z
-Commit: Patch security vulnerability in authentication
+Timestamp: 2025-11-06T11:33:03.519Z
+Commit: Fix memory leak in event handlers
