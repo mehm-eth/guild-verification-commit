f97343c3318343e33cbdbbdfd80eed36a8a92b42
mehmeth <137512931+mehm-eth@users.noreply.github.com>
2025-11-06 14:35:15 +0300

    Fix database connection timeout

--- helpers.ts
+++ helpers.ts
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2025-11-06T11:35:11.319Z
-Commit: Fix CSS styling bug on mobile devices
+Timestamp: 2025-11-06T11:35:14.806Z
+Commit: Fix database connection timeout
