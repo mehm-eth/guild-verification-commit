38e95ce29082614f91d04d42207b001aebee4ad3
mehmeth <137512931+mehm-eth@users.noreply.github.com>
2025-11-06 14:29:04 +0300

    Resolve null pointer exception in user service

--- utils.js
+++ utils.js
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2025-11-03T05:50:51.852Z
-Commit: Add export functionality for reports
+Timestamp: 2025-11-06T11:29:03.052Z
+Commit: Resolve null pointer exception in user service
