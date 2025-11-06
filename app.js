56d9d3bc88a327cc1f44c5fd470560e1907cfc19
mehmeth <137512931+mehm-eth@users.noreply.github.com>
2025-11-06 14:35:47 +0300

    Resolve null pointer exception in user service

--- app.js
+++ app.js
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2025-11-06T11:35:41.396Z
-Commit: Add multi-language support
+Timestamp: 2025-11-06T11:35:46.399Z
+Commit: Resolve null pointer exception in user service
