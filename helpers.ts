da1489830a120672406a15dc77de9c99673acb9a
mehmeth <137512931+mehm-eth@users.noreply.github.com>
2025-11-06 14:33:41 +0300

    Fix race condition in async operations

--- helpers.ts
+++ helpers.ts
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2025-11-06T11:33:06.774Z
-Commit: Implement user profile customization
+Timestamp: 2025-11-06T11:33:40.966Z
+Commit: Fix race condition in async operations
