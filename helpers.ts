73079a021406163f19ab7206eaa3272f59ebdc46
mehmeth <137512931+mehm-eth@users.noreply.github.com>
2025-11-06 14:28:47 +0300

    Fix broken links in documentation

--- helpers.ts
+++ helpers.ts
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2025-11-06T11:28:23.357Z
-Commit: Resolve timezone issues in date handling
+Timestamp: 2025-11-06T11:28:46.949Z
+Commit: Fix broken links in documentation
