a8b523fe2790ed0fe2d37f804a5229964357693e
mehmeth <137512931+mehm-eth@users.noreply.github.com>
2025-11-06 14:33:15 +0300

    Patch security vulnerability in authentication

--- utils.js
+++ utils.js
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2025-11-06T11:29:03.052Z
-Commit: Resolve null pointer exception in user service
+Timestamp: 2025-11-06T11:33:14.398Z
+Commit: Patch security vulnerability in authentication
