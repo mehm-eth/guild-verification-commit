75bc1e4b39842c669ccf8c43e1e0932f8458e20c
mehmeth <137512931+mehm-eth@users.noreply.github.com>
2025-11-06 14:35:01 +0300

    Patch API endpoint error handling

--- utils.js
+++ utils.js
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2025-11-06T11:33:43.511Z
-Commit: Fix broken links in documentation
+Timestamp: 2025-11-06T11:35:00.670Z
+Commit: Patch API endpoint error handling
