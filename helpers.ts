e9d4cbb0114486c586b446b72040ddd876324b3d
mehmeth <137512931+mehm-eth@users.noreply.github.com>
2025-11-06 14:37:13 +0300

    Patch security vulnerability in authentication

--- helpers.ts
+++ helpers.ts
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2025-11-06T11:37:10.680Z
-Commit: Add export functionality for reports
+Timestamp: 2025-11-06T11:37:12.489Z
+Commit: Patch security vulnerability in authentication
