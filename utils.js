b145c447c5441c4743b19b345d1f767740438b3f
mehmeth <137512931+mehm-eth@users.noreply.github.com>
2025-11-06 14:39:21 +0300

    Resolve null pointer exception in user service

--- utils.js
+++ utils.js
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2025-11-06T11:39:02.332Z
-Commit: Fix CSS styling bug on mobile devices
+Timestamp: 2025-11-06T11:39:20.740Z
+Commit: Resolve null pointer exception in user service
