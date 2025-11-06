fa95c11bea849e6b4ef95fcd1b2c3e3924933c06
mehmeth <137512931+mehm-eth@users.noreply.github.com>
2025-11-06 14:29:35 +0300

    Implement user profile customization

--- helpers.ts
+++ helpers.ts
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2025-11-06T11:28:46.949Z
-Commit: Fix broken links in documentation
+Timestamp: 2025-11-06T11:29:34.348Z
+Commit: Implement user profile customization
