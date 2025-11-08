ce1266e3c06468f10f6e5236ac0ead3e4e5193b2
mehmeth <137512931+mehm-eth@users.noreply.github.com>
2025-11-08 09:25:23 +0300

    Patch API endpoint error handling

--- utils.js
+++ utils.js
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2025-11-08T06:25:06.955Z
-Commit: Introduce real-time notifications
+Timestamp: 2025-11-08T06:25:21.168Z
+Commit: Patch API endpoint error handling
