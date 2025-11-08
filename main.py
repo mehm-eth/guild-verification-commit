66f3f703d5c0a838222867c50d7d0a7b573d2267
mehmeth <137512931+mehm-eth@users.noreply.github.com>
2025-11-08 09:15:42 +0300

    Fix broken links in documentation

--- main.py
+++ main.py
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2025-11-08T06:15:37.079Z
-Commit: Resolve null pointer exception in user service
+Timestamp: 2025-11-08T06:15:41.125Z
+Commit: Fix broken links in documentation
