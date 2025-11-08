d1d9ad9cb64b55b0fd2e623af5b56db261ac5cee
mehmeth <137512931+mehm-eth@users.noreply.github.com>
2025-11-08 09:19:51 +0300

    Resolve timezone issues in date handling

--- utils.js
+++ utils.js
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2025-11-08T06:16:48.069Z
-Commit: Resolve null pointer exception in user service
+Timestamp: 2025-11-08T06:19:49.268Z
+Commit: Resolve timezone issues in date handling
