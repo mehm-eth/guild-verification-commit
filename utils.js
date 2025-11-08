510002d1680331ef550043f2c3494aa11ab65fe1
mehmeth <137512931+mehm-eth@users.noreply.github.com>
2025-11-08 09:15:27 +0300

    Patch API endpoint error handling

--- utils.js
+++ utils.js
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2025-11-08T06:14:59.629Z
-Commit: Correct typo in configuration file
+Timestamp: 2025-11-08T06:15:25.493Z
+Commit: Patch API endpoint error handling
