1c14df585843bbd65777342054a84dda1c76edc5
mehmeth <137512931+mehm-eth@users.noreply.github.com>
2025-11-08 09:20:03 +0300

    Fix memory leak in event handlers

--- utils.js
+++ utils.js
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2025-11-08T06:19:49.268Z
-Commit: Resolve timezone issues in date handling
+Timestamp: 2025-11-08T06:20:01.460Z
+Commit: Fix memory leak in event handlers
