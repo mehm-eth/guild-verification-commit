88c4ab6e9f38db1d0019ec000cccd15069284567
mehmeth <137512931+mehm-eth@users.noreply.github.com>
2025-11-08 09:15:38 +0300

    Resolve null pointer exception in user service

--- main.py
+++ main.py
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2025-11-08T06:15:36.112Z
-Commit: Add multi-language support
+Timestamp: 2025-11-08T06:15:37.079Z
+Commit: Resolve null pointer exception in user service
