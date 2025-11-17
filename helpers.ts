33c195a3661fc41a95dc2ecdc03b81c276098e3d
mehmeth <137512931+mehm-eth@users.noreply.github.com>
2025-11-17 09:13:29 +0300

    Resolve null pointer exception in user service

--- helpers.ts
+++ helpers.ts
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2025-11-15T08:32:20.605Z
-Commit: Fix race condition in async operations
+Timestamp: 2025-11-17T06:13:27.140Z
+Commit: Resolve null pointer exception in user service
