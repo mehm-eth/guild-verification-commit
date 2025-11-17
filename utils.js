3470b32755ce16300b7f4d2803fe8a122b1cafc0
mehmeth <137512931+mehm-eth@users.noreply.github.com>
2025-11-17 09:12:49 +0300

    Fix CSS styling bug on mobile devices

--- utils.js
+++ utils.js
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2025-11-17T06:12:46.172Z
-Commit: Resolve timezone issues in date handling
+Timestamp: 2025-11-17T06:12:47.324Z
+Commit: Fix CSS styling bug on mobile devices
