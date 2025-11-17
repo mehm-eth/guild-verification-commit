5244b23d2d4f3e10e54076d98955d7565af455ca
mehmeth <137512931+mehm-eth@users.noreply.github.com>
2025-11-17 10:30:42 +0300

    Fix broken links in documentation

--- main.py
+++ main.py
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2025-11-17T07:30:19.211Z
-Commit: Patch security vulnerability in authentication
+Timestamp: 2025-11-17T07:30:40.210Z
+Commit: Fix broken links in documentation
