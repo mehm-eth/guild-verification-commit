2b8178323e3355738ccbb54d6200c4220eb90752
mehmeth <137512931+mehm-eth@users.noreply.github.com>
2025-11-20 10:47:19 +0300

    Fix broken links in documentation

--- app.js
+++ app.js
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2025-11-20T07:47:16.309Z
-Commit: Resolve null pointer exception in user service
+Timestamp: 2025-11-20T07:47:17.454Z
+Commit: Fix broken links in documentation
