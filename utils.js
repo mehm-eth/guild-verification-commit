be1405985715592cbee2061805628a464db48326
mehmeth <137512931+mehm-eth@users.noreply.github.com>
2025-11-20 10:47:20 +0300

    Patch security vulnerability in authentication

--- utils.js
+++ utils.js
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2025-11-20T07:47:03.042Z
-Commit: Fix CSS styling bug on mobile devices
+Timestamp: 2025-11-20T07:47:18.645Z
+Commit: Patch security vulnerability in authentication
