3c641a653718764e37c54fb77949a9d33da37e50
mehmeth <137512931+mehm-eth@users.noreply.github.com>
2025-11-21 10:37:33 +0300

    Resolve null pointer exception in user service

--- helpers.ts
+++ helpers.ts
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2025-11-21T07:37:00.842Z
-Commit: Resolve timezone issues in date handling
+Timestamp: 2025-11-21T07:37:31.223Z
+Commit: Resolve null pointer exception in user service
