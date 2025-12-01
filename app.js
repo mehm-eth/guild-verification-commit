2d37782308115870987677d26341032aaaa00ec3
mehmeth <137512931+mehm-eth@users.noreply.github.com>
2025-12-01 09:31:10 +0300

    Fix memory leak in event handlers

--- app.js
+++ app.js
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2025-12-01T06:17:44.782Z
-Commit: Resolve null pointer exception in user service
+Timestamp: 2025-12-01T06:31:06.688Z
+Commit: Fix memory leak in event handlers
