477af5e7888bd20063bfcf0e899000aa0d7c7d06
mehmeth <137512931+mehm-eth@users.noreply.github.com>
2025-12-01 09:17:54 +0300

    Fix broken links in documentation

--- utils.js
+++ utils.js
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2025-11-22T05:48:48.590Z
-Commit: Add export functionality for reports
+Timestamp: 2025-12-01T06:17:50.521Z
+Commit: Fix broken links in documentation
