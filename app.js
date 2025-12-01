a247ad1a406561b33c5d7588b31a18b6f001edc1
mehmeth <137512931+mehm-eth@users.noreply.github.com>
2025-12-01 09:16:52 +0300

    Fix broken links in documentation

--- app.js
+++ app.js
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2025-11-22T05:48:51.039Z
-Commit: Fix CSS styling bug on mobile devices
+Timestamp: 2025-12-01T06:16:49.797Z
+Commit: Fix broken links in documentation
