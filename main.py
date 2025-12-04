72f3ccf947f962f90c816accda38a30d38a9c62e
mehmeth <137512931+mehm-eth@users.noreply.github.com>
2025-12-04 16:20:07 +0300

    Add multi-language support

--- main.py
+++ main.py
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2025-12-04T06:34:29.991Z
-Commit: Resolve null pointer exception in user service
+Timestamp: 2025-12-04T13:20:07.022Z
+Commit: Add multi-language support
